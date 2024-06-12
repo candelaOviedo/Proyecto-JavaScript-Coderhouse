c482b2f54d7d182a4bdd0ef5948440db535cc9b7
Candela Oviedo <candelaoviedo1313@gmail.com>
2024-06-12 17:30:36 -0300

    Cambios js

--- main.js
+++ main.js
@@ -18,22 +18,57 @@ const excursiones = [
     {
         id: 1,
         destino: "Tour a Purmamarca",
-        descripcion: "algo del tour",
-        precioPorPersona: 30000,
+        descripcion: "Embárcate en un viaje inolvidable a Purmamarca, un encantador pueblo en la Quebrada de Humahuaca, Patrimonio de la Humanidad por la UNESCO. Maravíllate con el icónico Cerro de los Siete Colores, pasea por sus pintorescas calles y visita la histórica iglesia de Santa Rosa de Lima. Disfruta de la cultura local en el mercado artesanal y explora el impresionante Paseo de los Colorados. Experimenta la auténtica esencia del norte argentino en Purmamarca",
+        precioPorPersona: 45000,
         cantidadPersonas: 1
     },
     {
         id: 2,
         destino: "Tour a Cafayate",
-        descripcion: "algo del tour",
-        precioPorPersona: 35000,
+        descripcion: "Embárcate en una aventura inolvidable por un pintoresco pueblo situado en el corazón de los Valles Calchaquíes. Famoso por sus paisajes deslumbrantes y sus vinos de alta calidad, especialmente el Torrontés, Cafayate es el destino perfecto para los amantes de la naturaleza y la enología.",
+        precioPorPersona: 55000,
         cantidadPersonas: 1
     },
     {
         id: 3,
-        destino: "Tour a Tilcara",
-        descripcion: "algo del tour",
-        precioPorPersona: 25000,
+        destino: "Tour a Puerto Madryn",
+        descripcion: "Embárcate en una emocionante aventura a Puerto Madryn, un destino único en Argentina, conocido por su impresionante belleza natural y su vida marina abundante, que incluye la fascinante presencia de ballenas francas australes durante su temporada de avistaje. Descubre la magia de este pintoresco pueblo, donde el mar y la tierra se encuentran en armonía para ofrecerte experiencias inolvidables.",
+        precioPorPersona: 58000,
+        cantidadPersonas: 1
+    },
+    {
+        id: 4,
+        destino: "Cataratas del Iguazú",
+        descripcion: "Embárcate en una aventura inolvidable y descubre la majestuosa belleza de las Cataratas del Iguazú, un destino que te dejará sin aliento. Situadas en la frontera entre Argentina y Brasil, estas impresionantes cascadas han sido declaradas Patrimonio de la Humanidad por la UNESCO y son consideradas una de las Siete Maravillas Naturales del Mundo.",
+        precioPorPersona: 50000,
+        cantidadPersonas: 1
+    },
+    {
+        id: 5,
+        destino: "Tour a Bariloche",
+        descripcion: "Descubre el corazón de la Patagonia argentina, un destino que combina paisajes de ensueño, aventuras emocionantes y una rica cultura local. Situada a orillas del majestuoso Lago Nahuel Huapi y rodeada por los imponentes Andes, Bariloche es el lugar perfecto para los amantes de la naturaleza y las actividades al aire libre.",
+        precioPorPersona: 60000,
+        cantidadPersonas: 1
+    },
+    {
+        id: 6,
+        destino: "Glaciar Perito Moreno",
+        descripcion: "Embárcate en una aventura inolvidable al Glaciar Perito Moreno, uno de los tesoros más impresionantes de la Patagonia argentina. Situado en el Parque Nacional Los Glaciares, este colosal glaciar es famoso por su majestuosidad, sus espectaculares rupturas y su accesibilidad única, permitiéndote experimentar de cerca la maravilla de la naturaleza en su estado más puro.",
+        precioPorPersona: 58000,
+        cantidadPersonas: 1
+    },
+    {
+        id: 7,
+        destino: "Tour a Mendoza",
+        descripcion: "Embárcate en una aventura inolvidable al Glaciar Perito Moreno, uno de los tesoros más impresionantes de la Patagonia argentina. Situado en el Parque Nacional Los Glaciares, este colosal glaciar es famoso por su majestuosidad, sus espectaculares rupturas y su accesibilidad única, permitiéndote experimentar de cerca la maravilla de la naturaleza en su estado más puro.",
+        precioPorPersona: 53000,
+        cantidadPersonas: 1
+    },
+    {
+        id: 8,
+        destino: "Tour a Córdoba",
+        descripcion: "Embárcate en una aventura inolvidable al Glaciar Perito Moreno, uno de los tesoros más impresionantes de la Patagonia argentina. Situado en el Parque Nacional Los Glaciares, este colosal glaciar es famoso por su majestuosidad, sus espectaculares rupturas y su accesibilidad única, permitiéndote experimentar de cerca la maravilla de la naturaleza en su estado más puro.",
+        precioPorPersona: 48000,
         cantidadPersonas: 1
     }
 ];
