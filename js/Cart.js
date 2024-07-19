c621b230d7d4b72618442b59dca732bb8db395c3
Candela Oviedo <candelaoviedo1313@gmail.com>
2024-07-18 21:54:27 -0300

    cambios

--- js/Cart.js
+++ js/Cart.js
@@ -4,56 +4,73 @@ let totalGeneral = 0;
 
 const renderizarCarrito = () => {
 
-    contenedorCarrito.innerHTML = "";
-    totalGeneral = 0; // Reiniciar el total general al renderizar el carrito
+  contenedorCarrito.innerHTML = "";
+  totalGeneral = 0; // Reiniciar el total general al renderizar el carrito
 
-    carrito.forEach(({ id, destino, precioPorPersona, cantidad }) => {
-        const divCarrito = document.createElement("div");
+  carrito.forEach(({ id, destino, precioPorPersona, cantidad }) => {
+    const divCarrito = document.createElement("div");
 
-        let totalPorExcursion = precioPorPersona * cantidad;
-        totalGeneral += totalPorExcursion;
+    let totalPorExcursion = precioPorPersona * cantidad;
+    totalGeneral += totalPorExcursion;
 
 
-        divCarrito.innerHTML = `
+    divCarrito.innerHTML = `
             <div class="card-carrito">
             <p class="card-text-carrito">Cantidad: ${cantidad}</p>
             <p class="card-text-carrito">Excursión: ${destino}</p>
             <p class="card-text-carrito">Total: $ ${totalPorExcursion}</p>
             <a href="#" id="eliminar-${id}" class="btn btn-danger btn-carrito">Eliminar del carrito</a>
             `;
+    // Agregar evento click al botón de eliminar
+    const botonEliminar = divCarrito.querySelector(`#eliminar-${id}`);
 
-        // Agregar evento click al botón de eliminar
-        const botonEliminar = divCarrito.querySelector(`#eliminar-${id}`);
-        botonEliminar.addEventListener("click", (event) => {
-            event.preventDefault();
-            eliminarDelCarrito(id);
-        });
+    botonEliminar.addEventListener("click", (event) => {
+      event.preventDefault();
+      Swal.fire({
+        title: "Estás seguro que deseas eliminar la excursión del carrito?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar",
+        cancelButtonText: "Cancelar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          eliminarDelCarrito(id);
+          Swal.fire({
+            title: "Eliminado!",
+            text: "La excursión fue eliminada del carrito",
+            icon: "success"
+          });
+        }
+      });
+    });
 
-        contenedorCarrito.appendChild(divCarrito);
-    })
+    contenedorCarrito.appendChild(divCarrito);
+  })
 
-    // Guardar el carrito en localStorage
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+  // Guardar el carrito en localStorage
+  localStorage.setItem('carrito', JSON.stringify(carrito));
 
-    // Renderizar el total después de actualizar el carrito
-    renderizarTotal();
+  // Renderizar el total después de actualizar el carrito
+  renderizarTotal();
 }
 
 const eliminarDelCarrito = (idExcursion) => {
-    const index = carrito.findIndex(excursion => excursion.id === idExcursion);
+  const index = carrito.findIndex(excursion => excursion.id === idExcursion);
 
-    if (index !== -1) {
-        carrito[index].cantidad > 1 ? carrito[index].cantidad-- : carrito.splice(index, 1);
-    }
+  if (index !== -1) {
+    carrito[index].cantidad > 1 ? carrito[index].cantidad-- : carrito.splice(index, 1);
+  }
 
-    // Renderizar nuevamente el carrito actualizado
-    renderizarCarrito();
+  // Renderizar nuevamente el carrito actualizado
+  renderizarCarrito();
 
 }
 
 
 const renderizarTotal = () => {
-    contenedorTotalCarrito.innerHTML = `
+  contenedorTotalCarrito.innerHTML = `
             <div class="card-header-total"> Resumen </div>
             <hr>
                 <div class="total-compra">
@@ -62,10 +79,32 @@ const renderizarTotal = () => {
                 </div>
                 <div class="buttons-total-compra">
                 <a href= "../../index.html"><button type="button" class="btn btn-info btn-total">Seguir comprando</button></a>
-                <button type="button" class="btn btn-primary btn-total">Continuar compra</button>
+                <button type="button" id="btn-finalizar-compra" class="btn btn-primary btn-total">Comprar</button>
                 </div>
 `;
+  const btnFinalizarCompra = document.getElementById('btn-finalizar-compra');
+
+  btnFinalizarCompra.addEventListener('click', (e) => {
+    e.preventDefault();
+    Swal.fire({
+      title: "Está seguro de finalizar la compra?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, comprar",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Compra realizada con éxito!",
+          text: "Has finalizado la compra. Se te enviarán al mail las entradas correspondientes.",
+          icon: "success"
+        });
+      }
+    });
+  });
 }
 
-renderizarCarrito();
 
+renderizarCarrito();
--- js/Data.js
+++ js/Data.js
@@ -1,52 +1,52 @@
-    const excursiones = [
-        {
-            id: 1,
-            destino: "Tour a Puerto Madryn",
-            descripcion: "Embárcate en una emocionante aventura a Puerto Madryn, un destino único en Argentina, conocido por su impresionante belleza natural y su vida marina abundante, que incluye la fascinante presencia de ballenas francas australes durante su temporada de avistaje. Descubre la magia de este pintoresco pueblo, donde el mar y la tierra se encuentran en armonía para ofrecerte experiencias inolvidables.",
-            precioPorPersona: 58000,
-            img: "./assets/puertoMadryn.webp",
-            cantidadPersonas: 1
-        },
-        {
-            id: 2,
-            destino: "Tour a Purmamarca",
-            descripcion: "Embárcate en un viaje inolvidable a Purmamarca, un encantador pueblo en la Quebrada de Humahuaca, Patrimonio de la Humanidad por la UNESCO. Maravíllate con el icónico Cerro de los Siete Colores, pasea por sus pintorescas calles y visita la histórica iglesia de Santa Rosa de Lima. Disfruta de la cultura local en el mercado artesanal y explora el impresionante Paseo de los Colorados. Experimenta la auténtica esencia del norte argentino en Purmamarca",
-            precioPorPersona: 45000,
-            img: "./assets/purmamarca.jpg",
-            cantidadPersonas: 1
-        },
-        {
-            id: 3,
-            destino: "Tour a Bariloche",
-            descripcion: "Descubre el corazón de la Patagonia argentina, un destino que combina paisajes de ensueño, aventuras emocionantes y una rica cultura local. Situada a orillas del majestuoso Lago Nahuel Huapi y rodeada por los imponentes Andes, es el lugar perfecto para los amantes de la naturaleza y las actividades al aire libre. Bariloche te recibe con sus paisajes de postal, donde los picos nevados de las montañas se reflejan en las aguas cristalinas del lago.",
-            precioPorPersona: 60000,
-            img: "./assets/bariloche.jpg",
-            cantidadPersonas: 1
-        },
-        {
-            id: 4,
-            destino: "Valle de la luna",
-            descripcion: "Descubre el misterioso y fascinante Valle de la Luna, ubicado en el Parque Provincial Ischigualasto en la provincia de San Juan. Este paisaje surrealista, con formaciones rocosas únicas y una geología impresionante, te transportará a otra época. Explora este desierto lunar, declarado Patrimonio de la Humanidad por la UNESCO, donde los colores y las formas de las rocas cuentan la historia de millones de años.",
-            precioPorPersona: 62000,
+const excursiones = [
+    {
+        id: 1,
+        destino: "Tour a Puerto Madryn",
+        descripcion: "Embárcate en una emocionante aventura a Puerto Madryn, un destino único en Argentina, conocido por su impresionante belleza natural y su vida marina abundante, que incluye la fascinante presencia de ballenas francas australes durante su temporada de avistaje. Descubre la magia de este pintoresco pueblo, donde el mar y la tierra se encuentran en armonía para ofrecerte experiencias inolvidables.",
+        precioPorPersona: 58000,
+        img: "./assets/puertoMadryn.webp",
+        cantidadPersonas: 1
+    },
+    {
+        id: 2,
+        destino: "Tour a Purmamarca",
+        descripcion: "Embárcate en un viaje inolvidable a Purmamarca, un encantador pueblo en la Quebrada de Humahuaca, Patrimonio de la Humanidad por la UNESCO. Maravíllate con el icónico Cerro de los Siete Colores, pasea por sus pintorescas calles y visita la histórica iglesia de Santa Rosa de Lima. Disfruta de la cultura local en el mercado artesanal y explora el impresionante Paseo de los Colorados. Experimenta la auténtica esencia del norte argentino en Purmamarca",
+        precioPorPersona: 45000,
+        img: "./assets/purmamarca.jpg",
+        cantidadPersonas: 1
+    },
+    {
+        id: 3,
+        destino: "Tour a Bariloche",
+        descripcion: "Descubre el corazón de la Patagonia argentina, un destino que combina paisajes de ensueño, aventuras emocionantes y una rica cultura local. Situada a orillas del majestuoso Lago Nahuel Huapi y rodeada por los imponentes Andes, es el lugar perfecto para los amantes de la naturaleza y las actividades al aire libre. Bariloche te recibe con sus paisajes de postal, donde los picos nevados de las montañas se reflejan en las aguas cristalinas del lago.",
+        precioPorPersona: 60000,
+        img: "./assets/bariloche.jpg",
+        cantidadPersonas: 1
+    },
+    {
+        id: 4,
+        destino: "Valle de la luna",
+        descripcion: "Descubre el misterioso y fascinante Valle de la Luna, ubicado en el Parque Provincial Ischigualasto en la provincia de San Juan. Este paisaje surrealista, con formaciones rocosas únicas y una geología impresionante, te transportará a otra época. Explora este desierto lunar, declarado Patrimonio de la Humanidad por la UNESCO, donde los colores y las formas de las rocas cuentan la historia de millones de años.",
+        precioPorPersona: 62000,
         img: "./assets/valleLuna.webp",
         cantidadPersonas: 1
     },
-        {
-            id: 5,
-            destino: "Cataratas del Iguazú",
-            descripcion: "Embárcate en una aventura inolvidable y descubre la majestuosa belleza de las Cataratas del Iguazú, un destino que te dejará sin aliento. Situadas en la frontera entre Argentina y Brasil, estas impresionantes cascadas han sido declaradas Patrimonio de la Humanidad por la UNESCO y son consideradas una de las Siete Maravillas Naturales del Mundo.",
-            precioPorPersona: 50000,
-            img: "./assets/cataratas.jpg",
+    {
+        id: 5,
+        destino: "Cataratas del Iguazú",
+        descripcion: "Embárcate en una aventura inolvidable y descubre la majestuosa belleza de las Cataratas del Iguazú, un destino que te dejará sin aliento. Situadas en la frontera entre Argentina y Brasil, estas impresionantes cascadas han sido declaradas Patrimonio de la Humanidad por la UNESCO y son consideradas una de las Siete Maravillas Naturales del Mundo.",
+        precioPorPersona: 50000,
+        img: "./assets/cataratas.jpg",
         cantidadPersonas: 1
     },
     {
         id: 6,
         destino: "Glaciar Perito Moreno",
         descripcion: "Una aventura inolvidable al Glaciar Perito Moreno, uno de los tesoros más impresionantes de la Patagonia argentina. Situado en el Parque Nacional Los Glaciares, este colosal glaciar es famoso por su majestuosidad, sus espectaculares rupturas y su accesibilidad única, permitiéndote experimentar de cerca la maravilla de la naturaleza en su estado más puro.",
-    precioPorPersona: 69000,
-    img: "assets/glaciarPeritoMoreno.jpg",
-    cantidadPersonas: 1
-},
+        precioPorPersona: 69000,
+        img: "assets/glaciarPeritoMoreno.jpg",
+        cantidadPersonas: 1
+    },
     {
         id: 7,
         destino: "Tour a Mendoza",
@@ -71,5 +71,4 @@
         img: "./assets/cafayate.jpg",
         cantidadPersonas: 1
     },
-    
 ];
